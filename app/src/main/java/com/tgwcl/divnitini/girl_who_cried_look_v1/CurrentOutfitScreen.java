fdeb422bc600d138200adc2ffded57994d71e026
nitini <nitin.iyer.1103@gmail.com>
2017-01-31 23:29:20 -0800

    First commit, sending to github

---
+++ app/src/main/java/com/tgwcl/divnitini/girl_who_cried_look_v1/CurrentOutfitScreen.java
@@ -0,0 +1,13 @@
+package com.tgwcl.divnitini.girl_who_cried_look_v1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CurrentOutfitScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_outfit_screen);
+    }
+}
